4186b56cba501b0f87987988ea6a9d3f48f29357
Mpicea <100979640+Mpicea@users.noreply.github.com>
2022-06-17 17:07:28 +0900

    Add files via upload

---
+++ socServer/CClientSocket.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+//#define WM_CLIENT_IPADDRESS WM_USER+2
+#define WM_CLIENT_MSG_RECV WM_USER+2
+#define WM_CLIENT_CLOSE WM_USER+3
+
+// CClientSocket 명령 대상
+
+class CClientSocket : public CSocket
+{
+public:
+	CClientSocket();
+	virtual ~CClientSocket();
+	void SetWnd(HWND hWnd);
+	HWND m_hWnd;
+	virtual void OnReceive(int nErrorCode);
+	virtual void OnClose(int nErrorCode);
+};
+
+
